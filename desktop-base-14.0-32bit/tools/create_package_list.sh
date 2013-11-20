47f20a79e191f9020eb6f4ce237bc8d030a979ef
Niki Kovacs <info@microlinux.fr>
2013-11-20 12:03:52 +0100

    ...

--- desktop-base-14.0-32bit/tools/create_package_list.sh
+++ desktop-base-14.0-32bit/tools/create_package_list.sh
@@ -4,5 +4,7 @@ CWD=$(pwd)
 ls $CWD/../../desktop-base-14.0-source \
   > /tmp/all.txt
 cat /tmp/all.txt \
+  | grep -v all.SlackBuild \
+  | grep -v build_order \
   | grep -v openoffice-langpack \
   > $CWD/pkglists/packages-desktop-base
