0483ac04c7a99549b2aa75549b179e693eb04ca8
Niki Kovacs <info@microlinux.fr>
2013-12-31 08:37:06 +0100

    ...

---
+++ MLED-14.1-32bit/tools/create_package_list.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+
+CWD=$(pwd)
+SOURCEDIR="$CWD/../../MLED-14.1-source"
+TEMPFILE="/tmp/all.txt"
+
+echo "#####################" > $TEMPFILE
+echo "# MLED package list #" >> $TEMPFILE
+echo "#####################" >> $TEMPFILE
+
+for PKGGROUP in ap d gnome l locale multimedia n profile x xap xfce; do
+  echo >> $TEMPFILE
+  echo "# $PKGGROUP" >> $TEMPFILE
+  ls $SOURCEDIR/$PKGGROUP \
+  >> $TEMPFILE
+done
+cat $TEMPFILE \
+  | grep -v openoffice-langpack \
+  | grep -v filezilla \
+  | grep -v virtualbox \
+  > $CWD/pkglists/packages-MLED
+
